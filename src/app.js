d21461a0d5c6a97ae81c0532ef12b831f73fc5f2
valeriascocozzag <valeriascocozzag@gmail.com>
2025-03-16 19:45:05 +0000

    vs-sxcusesgenerator.4g

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+function getRandomArray(array) {
+  let max=array.length;
+  let position=Math.floor(Math.random() * max);
+  return array[position];
+}
+
+window.onload = function() { 
+  let who = ['El perro', 'Mi vecina', 'El gato', 'Mi papá']
+  let action = ['se comió', 'botó', 'aplastó', 'rompió']
+  let what = ['mi tarea', 'el teléfono', 'la taza de café', 'los lentes']
+  let when = ['antes de clases', 'mientras dormía','hace una semana', 'en el jardín']
+
+  let frase=getRandomArray(who) + ' ' + getRandomArray(action) + ' ' + getRandomArray(what) + ' ' + getRandomArray(when); 
+  document.getElementById("excuse").innerHTML = frase;
+  document.getElementById("titulo").innerHTML = "Oh no! No sabes lo que pasó....";
 };
